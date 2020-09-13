215c623bdbcc7426abe95784559b549d4984f1c4
Tapas Mandal <tapas@alloymobile.ca>
2020-09-13 00:51:23 -0400

    YL00-1000#Update the basic layout

--- blog/src/app/model/object.model.ts
+++ blog/src/app/model/object.model.ts
@@ -12,16 +12,45 @@ export class Category {
   }
 }
 
+export class CommentUser {
+  id: number;
+  name: string;
+  constructor(commentuser?: any) {
+    if (commentuser) {
+      this.id = commentuser.id;
+      this.name = commentuser.name;
+    } else {
+      this.id = 0;
+      this.name = '';
+    }
+  }
+}
+
 export class Post {
   id: number;
   title: string;
+  details: string;
+  poster: string;
+  image: string;
+  date: string;
+  likes: string;
   constructor(post?: any) {
     if (post) {
       this.id = post.id;
       this.title = post.title;
+      this.details = post.details;
+      this.poster = post.poster;
+      this.image = post.image;
+      this.date = post.date;
+      this.likes = post.likes;
     } else {
       this.id = 0;
       this.title = '';
+      this.details = '';
+      this.poster = '';
+      this.image = '';
+      this.date = '';
+      this.likes = '';
     }
   }
 }
@@ -43,6 +72,7 @@ export const TableName: any = {
   Category,
   Post,
   Tag,
+  CommentUser,
 };
 
 export class Table {
